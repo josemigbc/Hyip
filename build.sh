5da43e341a0778b71534ed6f67bd0382afaa130d
Jose_Miguel <apolo13x@gmail.com>
2023-08-25 11:53:23 -0400

    Fixing build

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py makemigrations
+python manage.py makemigrations authentication
+python manage.py makemigrations deposits
+python manage.py makemigrations plans
+python manage.py makemigrations withdraw
 python manage.py migrate
